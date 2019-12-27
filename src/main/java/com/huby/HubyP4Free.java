6996c6f56f29c602c1110f37324dec89f2836aa3
Tuka <htyns123>
2019-12-27 20:47:58 -0200

    First Commit

---
+++ src/main/java/com/huby/HubyP4Free.java
@@ -0,0 +1,17 @@
+package com.huby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HubyP4Free extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
