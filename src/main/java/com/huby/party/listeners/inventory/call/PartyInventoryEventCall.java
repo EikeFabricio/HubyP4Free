277b83b4c097a00b2db4ba6576abed5b81ae5a78
Eike Fabrício <fabricioeike978@gmail.com>
2019-12-28 17:09:08 -0300

    files added

--- src/main/java/com/huby/party/listeners/inventory/call/PartyInventoryEventCall.java
+++ src/main/java/com/huby/party/listeners/inventory/call/PartyInventoryEventCall.java
@@ -1,4 +1,29 @@
 package com.huby.party.listeners.inventory.call;
 
-public class PartyInventoryEventCall {
+import com.huby.party.listeners.inventory.custom.PartyInventoryClickEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class PartyInventoryEventCall implements Listener {
+
+    @EventHandler
+    void onClick(InventoryClickEvent e) {
+        if (e.getCurrentItem() == null) return;
+        if (e.getCurrentItem().getType() == null) return;
+        if (e.getCurrentItem().getType() == Material.AIR) return;
+
+        if (e.getInventory().getName().equals("Party")) {
+            e.setCancelled(true);
+
+            PartyInventoryClickEvent partyInventoryClickEvent = new PartyInventoryClickEvent(e.getCurrentItem(),
+                    e.getInventory(), e.getSlot(), (Player) e.getWhoClicked());
+
+            Bukkit.getPluginManager().callEvent(partyInventoryClickEvent);
+        }
+
+    }
 }
--- src/main/java/com/huby/party/listeners/inventory/custom/PartyInventoryClickEvent.java
+++ src/main/java/com/huby/party/listeners/inventory/custom/PartyInventoryClickEvent.java
@@ -1,4 +1,37 @@
 package com.huby.party.listeners.inventory.custom;
 
-public class PartyInventoryClickEvent {
+import com.huby.party.events.model.PartyEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class PartyInventoryClickEvent extends PartyEvent {
+
+    private final ItemStack currentItem;
+    private final Inventory inventory;
+    private final int slot;
+    private final Player player;
+
+    public PartyInventoryClickEvent(ItemStack currentItem, Inventory inventory, int slot, Player player) {
+        this.currentItem = currentItem;
+        this.inventory = inventory;
+        this.slot = slot;
+        this.player = player;
+    }
+
+    public ItemStack getCurrentItem() {
+        return currentItem;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public int getSlot() {
+        return slot;
+    }
 }
